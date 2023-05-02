d937d859aeca5c95e3ffdaf744a1b7df524d4e4c
Neakhai Yauheni <beonezero@protonmail.com>
2023-05-02 15:00:43 +0300

    redact App, create: Header, Navbar, Profile

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className={"header"}>
+        <img src="https://cdn.icon-icons.com/icons2/2351/PNG/512/logo_vk_vkontakte_icon_143187.png" alt=""/>
+    </header>
+    )
+}
+
+export default Header
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className={"content"}>
+            <div>
+                <img
+                    src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+                    alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my post
+                <div>
+                    NEW post
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile
