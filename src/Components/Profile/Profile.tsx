60c4f0eb82373076596e86e23d458be0bc4f6832
Neakhai Yauheni <beonezero@protonmail.com>
2023-07-12 23:53:30 +0300

    67 lesson redirect

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import {MyPostsContainer} from "./MyPosts/MyPostsContainer";
 import {mapStateToPropsType} from "./ProfileContainer";
-import {Redirect} from "react-router-dom";
 
 
 const Profile = (props: mapStateToPropsType) => {
